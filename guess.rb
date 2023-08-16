c54530bd3ca547570d2bc618c377bd15b9e5db7c
Pame85 <pamellachua85@gmail.com>
2023-08-16 11:07:54 +0800

    guess function retart yes no loop

--- guess.rb
+++ guess.rb
@@ -1,26 +1,40 @@
-"Welcome To The Guessing Game !"
-secret_num = rand(1...100)
-#puts secret_numb
-puts "Welcome To The Guessing Game !"
+loop do
 
-tries = 1
-chances = 6
-guesses =[]
+	"Welcome To The Guessing Game !"
+	secret_num = rand(1...100)
+	#puts secret_numb
+	puts "Welcome To The Guessing Game !"
 
-while tries < chances
-	puts "Guess a number between 1 to 100"
-	answer = gets.to_i
+	secret_num = 88
+	tries = 1
+	chances = 6
+	guesses =[]
 
-if answer > secret_num
-	puts "Number is too High"
-elsif answer < secret_num
-	puts "Number is too Low"
-else answer = secret_num
-	puts "Answer is correct"
+	while tries <= chances do
+		puts "Guess a number between 1 to 100"
+		answer = gets.to_i
+
+	if answer > secret_num
+		puts "Number is too High"
+	elsif answer < secret_num
+		puts "Number is too Low"
+	else answer = secret_num
+		puts "Answer is correct"
 	break
- end
+ 	end
 
- guesses << answer
- puts "guesses: #{guesses}"
- 
+ 		guesses << answer
+ 		puts "guesses: #{guesses}"
+ 		tries +=1
+ 	end
+
+ 		puts "Do you want to continue"
+ 		puts "Y - Yes"
+ 		puts "N - No"
+
+ 		play = gets.chop.downcase
+ 		break if play != "y"
+
+
+ 	
 end
