2a6e833843d919d499de88791e77ea0e76843aba
Pame85 <pamellachua85@gmail.com>
2023-08-17 12:35:34 +0800

    array uniq

--- array_sum.rb
+++ array_sum.rb
@@ -1,7 +1,11 @@
-array = [1,2,3,4,5]
+array = [1,2,3,4,5,5,1]
+puts "Array Listing: #{array}"
 sum = 0
 array.each { |a| sum+=a }
-puts sum
+puts "Sum Value: #{sum}"
 
 max_val = array.max
 puts "Maximum value: #{max_val}" 
+
+array = array.uniq
+puts "Array Uniq: #{array.uniq}"
